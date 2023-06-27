8887565f9c043e5dc4ecd95202a9a81f2e145080
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-06-27 01:51:46 -0300

    contact form complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,37 @@ const Contact = () => {
   });
   const [loading, setLoading ] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({...form, [name]: value })
+    // emailjs.send({
+    //   from_email: form.name,
+    //   to_name: "Raphael",
+    //   from_email: form.email,
+    //   to_email: "contato@raphaelamericano.com.br",
+    //   message: form.message
+    // },
+    // 'public_key'
+    // ).then(() => {
+    //   setLoading(false);
+    //   alert('Thank you. I will get back to you as soon as possible.')
+    //   setForm({
+    //     name: "",
+    //     email: "",
+    //     message: ""
+    //   })
+    // }, (error) => {
+    //   setLoading(false);
+    //   console.log(error);
+    //   alert("Something went wrong")
+    // })
+  }
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // emailjs.send()
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
